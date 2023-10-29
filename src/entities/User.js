97a0a5d6fd7afa72597ccae9498ed823f727f1f1
Jancker Sepúlveda <sepulved.janck@gmail.com>
2023-10-29 00:26:21 -0400

    [ADD]:aditionals

---
+++ src/entities/User.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const MysqlConnection = require("../config/MysqlConnection");
+
+class User extends MysqlConnection {
+
+}
+
+module.exports = User;
---
+++ src/exchange/SaleInvoiceStates.enum.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const SaleInvoiceStatesEnum = {
+    APPROVED: "APPROVED",
+    REJECTED: "REJECTED",
+    PENDING: "PENDING"
+};
+
+module.exports = SaleInvoiceStatesEnum;
